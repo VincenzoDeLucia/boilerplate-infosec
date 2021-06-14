5e082ca8a5abee1f7254600d45046c352db94e15
Vincenzo De Lucia <vincenzo.delucia@icloud.com>
2021-06-14 23:35:06 +0200

    Require helmet

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+let helmet = require("helmet");
+
 module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
